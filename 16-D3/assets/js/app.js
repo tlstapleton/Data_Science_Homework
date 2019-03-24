e36c76c7b00604228ff50d06eecf28ca957ee0a1
Toni Stapleton <tonistpltn25@gmail.com>
2019-03-24 17:28:57 -0500

    make labels under the axes

--- 16-D3/assets/js/app.js
+++ 16-D3/assets/js/app.js
@@ -1,6 +1,6 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+var margin = {top: 20, right: 20, bottom: 50, left: 70},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 600 - margin.top - margin.bottom;
 
 
 var xValue = function(d) { return d.poverty;}, 
@@ -46,24 +46,29 @@ d3.csv("assets/data/data.csv", function(error, data) {
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
+ 
+
+svg.append("text")  
+      .attr("class", "label")           
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 20) + ")")
+      .style("text-anchor", "middle")
       .text("% In Poverty");
 
   // y-axis
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
-    .append("text")
-      .attr("class", "label")
+    
+svg.append("text")
+      .attr("class","label")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("% Lacks Healthcare");
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("% Lacking Healthcare"); 
 
  
 
